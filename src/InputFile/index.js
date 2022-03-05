2313f19bf4b0c475b7fe659659ecf0543437af46
Bruno Moura <bruno8moura@gmail.com>
2022-03-05 15:33:53 -0300

    refactor: change to nodejs stream

--- src/InputFile/index.js
+++ src/InputFile/index.js
@@ -1,20 +1,54 @@
 const crypto = require('crypto')
-const { writeFile, readFile, rm } = require('fs/promises')
+const { rm } = require('fs/promises')
+const fs = require('fs')
+
+const { promisify } = require('util')
+const { Readable, pipeline } = require('stream')
+const pipelineAsync = promisify(pipeline)
 class InputFile {
     constructor(name) {
         this.name = name
+        this.size = BigInt(0)
     }
 
     async create() {
-        await writeFile(this.name, crypto.randomBytes(1e9))
+        const readableStream = Readable({
+            read(){
+                const finished = null
+                const oneGb = 1e9
+                const threeGb = [ oneGb, oneGb, oneGb ]
+
+                for (let i = 0; i < threeGb.length; i++) {
+                    const data = crypto.randomBytes(threeGb[i])
+                    this.push(data)
+                }
+
+                this.push(finished)
+            }
+        })
+
+        await pipelineAsync(
+            readableStream,
+            fs.createWriteStream(this.name)
+        )
+    }
+
+    async length() {
+        return new Promise( ( resolve, reject ) => {
+            fs.createReadStream(this.name).on('data', (chunk) => {
+                this.size = this.size + BigInt(chunk.length)
+            })
+            .on('end', () => resolve(this.size))
+            .on('error', (e) => reject(e))            
+        })
     }
 
     async exists() {
         try {
-            const file = await readFile(this.name)    
-            return undefined !== file            
+            return (await this.length()) > 0
         } catch (error) {
             //log here
+            console.log(error)
         }
 
         return false
