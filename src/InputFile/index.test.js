330b3a0b5035792c8fa2cfd5a9c1ce1883a3afe5
Bruno Moura <bruno8moura@gmail.com>
2022-03-06 11:57:39 -0300

    test: 'create()' only creates an empty file

--- src/InputFile/index.test.js
+++ src/InputFile/index.test.js
@@ -1,31 +1,37 @@
 const chai = require('chai')
 const InputFile = require('../InputFile')
 
+const folder = 'temp'
 describe('InputFile', function() {
     this.timeout(Infinity)
         
-    it('should create file', async () => {
-        const fileName = `big.file-${Date.now()}`
-        const expected = true
-        const inputFile = new InputFile(fileName)
+    it('should create empty file', async () => {
+        const fileName = `emptyfile-${Date.now()}`
+        const expected = {
+            file: {
+                created: true
+            }
+        }
+
+        const inputFile = new InputFile({folder, name: fileName})
         await inputFile.create()
 
-        chai.assert((await inputFile.exists()) === expected)
+        chai.assert((await inputFile.exists()) === expected.file.created)
     })
 
     it('should delete file', async () => {
         const fileName = `big.file-${Date.now()}`
         const expected = {
             fileCreated: true,
-            fileDeleted: false
+            fileDeleted: true
         }
         
-        const inputFile = new InputFile(fileName)
+        const inputFile = new InputFile( { folder, name: fileName } )
         
         await inputFile.create()        
         chai.assert((await inputFile.exists()) === expected.fileCreated)
         
         await inputFile.delete()
-        chai.assert((await inputFile.exists()) === expected.fileDeleted)
+        chai.assert((!await inputFile.exists()) === expected.fileDeleted)
     })
 })
