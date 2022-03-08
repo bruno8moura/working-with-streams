5fc6c2f183f1a15d9f710432016de37ed4212b87
Bruno Moura <bruno8moura@gmail.com>
2022-03-08 05:12:45 -0300

    refactor: right identation

--- src/File/index.test.js
+++ src/File/index.test.js
@@ -36,9 +36,10 @@ describe('File', function () {
                     const person = { id: Date.now() + index, name: `Bruno-${index}` }
                     flush(person)
                 }
-            })
+            }
+        )
 
-        const file = new File({ folder, name: fileName, extension: fileType.JSON } )
+        const file = new File({ folder, name: fileName, extension: fileType.JSON })
         await file.create(streamContent)
 
         chai.assert((await file.exists()) === expected.file.created)
@@ -64,18 +65,18 @@ describe('File', function () {
         const fileName = `file-not-exists-${Date.now()}`
 
         const file = new File({ folder, name: fileName })
-        
+
         chai.expect((await file.length())).to.be.deep.equal(undefined)
-        
+
     })
 
     it("should return 'false' when file not exists", async () => {
         const fileName = `file-not-exists-${Date.now()}`
         const expected = false
-        const file = new File({ folder, name: fileName }, )
-        
+        const file = new File({ folder, name: fileName },)
+
         chai.expect((await file.exists())).to.be.deep.equal(expected)
-        
+
     })
 
     it("should return file size when there is a very large file", async () => {
@@ -87,12 +88,12 @@ describe('File', function () {
                     const _1gb_data = crypto.randomBytes(1e9)
                     flush(_1gb_data)
                 }
-            })
+            }
+        )
 
-        const file = new File({ folder, name: fileName } )
+        const file = new File({ folder, name: fileName })
         await file.create(streamContent)
-        
-        chai.expect((await file.length())).to.be.deep.equal(3000000000)        
+
+        chai.expect((await file.length())).to.be.deep.equal(3000000000)
     })
-    
 })
