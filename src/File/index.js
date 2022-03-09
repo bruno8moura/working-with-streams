4704b8a3e63e17a7f74f2fc1564a50171b24ab82
Bruno Moura <bruno8moura@gmail.com>
2022-03-09 05:41:19 -0300

    feat: append data into an existing file

--- src/File/index.js
+++ src/File/index.js
@@ -7,7 +7,7 @@ const pipelineAsync = promisify(pipeline)
 const NUMBER = require('../constants/NUMBER')
 const { BINARY } = require('./contentType')
 class File {
-    constructor( { folder, name, contentType=BINARY } ) {
+    constructor({ folder, name, contentType = BINARY }) {
         this.name = name
         this.folder = folder
         this.size = NUMBER.ZERO_BIGINT
@@ -16,23 +16,24 @@ class File {
     }
 
     async create(streamContent) {
-        await this.#write(streamContent)  
+        await this.#write(streamContent)
     }
 
-    async #write( streamContent ){
+    async #write(streamContent, append=false) {
+        const flags = append ? 'a' : 'w'
         const fileContent = this.contentType
         const readableStream = Readable({
-            read(){
-                if(streamContent){
-                    const flush = (data) => { 
+            read() {
+                if (streamContent) {
+                    const flush = (data) => {
                         const content = fileContent === BINARY ? data : JSON.stringify(data)
                         const encoding = fileContent === BINARY ? undefined : 'utf-8'
                         this.push(content, encoding)
                     }
 
-                    streamContent(flush)                    
+                    streamContent(flush)
                 }
-                
+
                 const finished = null
                 this.push(finished)
             }
@@ -40,14 +41,14 @@ class File {
 
         await pipelineAsync(
             readableStream,
-            fs.createWriteStream(this.filePath)
+            fs.createWriteStream(this.filePath, { flags })
         )
     }
 
     async length() {
         try {
-            const {size} = fs.statSync(this.filePath)
-            return size            
+            const { size } = fs.statSync(this.filePath)
+            return size
         } catch (error) {
             // implement logs
         }
@@ -63,6 +64,11 @@ class File {
     async delete() {
         await rm(this.filePath)
     }
+
+    async append(streamContent) {
+        const append = await this.exists() ? true : false
+        await this.#write(streamContent, append)
+    }
 }
 
-module.exports=File
+module.exports = File
