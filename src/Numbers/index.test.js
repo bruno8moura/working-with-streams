480b67b5e995cd51f49177c144394ec33247d862
Bruno Moura <bruno8moura@gmail.com>
2022-03-12 04:33:49 -0300

    test: ensure constant ZERO_BIGINT has value BigInt(0)

---
+++ src/Numbers/index.test.js
@@ -0,0 +1,13 @@
+const sut = require('../Numbers')
+
+const { describe, it } = require('mocha')
+const chai = require('chai')
+
+
+describe('Numbers', function() {
+    this.timeout(Infinity)
+
+    it('should ensure constant ZERO_BIGINT has value BigInt(0)', () => {
+        chai.assert(sut.ZERO_BIGINT === BigInt(0))
+    })
+})
